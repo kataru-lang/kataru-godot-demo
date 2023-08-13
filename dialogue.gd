4e2c8d3707c80c90dc3465445b968245602dad3c
Josiah Putman <joshikatsu@gmail.com>
2023-08-12 18:08:29 -0700

    Pass structured information via JSON

--- dialogue.gd
+++ dialogue.gd
@@ -3,11 +3,19 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	KataruManager.goto_passage("Start")
-	KataruManager.next("")
-	KataruManager.next("")
+	Kataru.choices.connect(_on_choices)
+	Kataru.goto_passage("Start")
+	Kataru.next("")
+	Kataru.next("")
+	Kataru.next("")
+	Kataru.next("Yes")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_choices(choices: Array, _timeout: float):
+	for choice in choices:
+		print("Choice: ", choice)
